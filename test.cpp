847a5f2d9fcce9f6546eaaf66859b372412a8c77
YMelon <929354023@qq.com>
2017-08-13 21:32:15 +0800

    implement the command <Gitlet init>

---
+++ test.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: Melon
+	> Mail:ymelon7@gmail.com 
+	> Created Time: Sun 13 Aug 2017 09:07:40 PM CST
+ ************************************************************************/
+
+#include "Gitlet.hpp"
+
+using namespace std;
+
+int main (int argc, const char *argv[])
+{
+    Gitlet gitlet;
+    gitlet.init();
+
+    return 0 ;
+}
